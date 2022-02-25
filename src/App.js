d5af24b908766a4b095c4ff4e9703f61299440ad
Matheus <tecolino3101@gmail.com>
2022-02-24 22:48:13 -0300

    finalizada calculadora e folha de estilo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+export default class Calc extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    result: 0,
+    num1: null,
+    num2: null
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      num1: Number(e.target.value)
+    })
+  }
+
+  handleChange2 = (e) => {
+    this.setState({
+      num2: Number(e.target.value)
+    })
+  }
+
+  minus = () => {
+    let {num1, num2} = this.state
+    if(num1 && num2 !== ""){
+    this.setState({
+      result: (num1) - (num2)
+    })
+  }
 }
+  multiply = () => {
+    let {num1, num2} =this.state
+    if(num1 && num2 !== ""){
+      this.setState({
+        result: num1 * num2
+      })
+    }
+  }
+  divide = () => {
+    let {num1, num2} = this.state
+    if(num1 && num2 !== ""){
+    this.setState({
+      result: (num1) / (num2)
+    })
+  }
+  }
+  add = () => {
+    let {num1, num2} = this.state
+    if(num1 && num2 !== ""){
+    this.setState({
+      result: (num1) + (num2)
+    })
+  }
+  }
 
-export default App;
+  clear = () => {
+    let {num1, num2} = this.state
+    if (num1, num2){
+    this.setState({
+      result: null,
+      num1: 0,
+      num2: 0
+    })
+  }else{
+    this.setState({
+      result: "Nenhum valor foi digitado"
+    })
+  }
+  }
+
+  render(){
+    return(
+      <div className='container'>
+        <div className="bodyCalc">
+      <h1>React Calc App</h1>
+        <div className='numberBox'>
+        <input 
+        value={this.state.num1} onChange={this.handleChange} type="number"/>
+        <div className='calc-button'>
+        <button onClick={this.minus}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
+        <button onClick={this.add}>+</button>
+        </div>
+        <input value={this.state.num2} onChange={this.handleChange2} type="number"/>
+        <button className="clearButton" onClick={this.clear}>clear</button>
+        </div>
+      <h2>{this.state.result}</h2>
+      </div>
+    </div>
+    )
+  }
+}
